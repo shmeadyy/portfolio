0c3073000927960b63439bc338ea65441cc2ce9c
Laura Mead <lauraemead7@gmail.com>
2014-10-16 16:02:51 -0700

    added image, updated toggle function in js, rearranged desktop menu

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
 	$('.mobile-menu ul').hide();
 
 	$('.mobile-menu h3').click(function(){
-		$('.mobile-menu ul').toggle();
+		$('.mobile-menu ul').slideToggle("slow");
 	})
 })
